4f05817cd673f8de48a2b54fe65caed77c9182ca
mcclin53 <dan.mcclintic@gmail.com>
2025-04-23 23:11:42 -0400

    updated user model to include virtual property

--- models/user.ts
+++ models/user.ts
@@ -1,15 +1,36 @@
-import { Schema, DOcument, model, ObjectId } from 'mongoose';
+import { Schema, Document, model, ObjectId } from 'mongoose';
+import { isEmail } from 'validator'
 
 interface IUser extends Document {
     _id: ObjectId;
     username: string;
     email: string;
-    thoughts: [];
-    friends: [];
+    thoughts: ObjectId[];
+    friends: ObjectId[];
+    friendCount?: number;
 }
 
-const friendCountSchema = new Schema<IUser>({
-    friendCount: Number,
-})
+const userSchema = new Schema<IUser>(
+    {
+        username: { type: String, required: true, unique: true, trim: true },
+        email: { type: String, required: true, unique: true, validate: [isEmail, 'Please fill a valid email address']},
+        thoughts: [{ type: ObjectId, ref: 'Thought' }],
+        friends: [{ type: ObjectId, ref: 'User' }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
-function friendCount = friends.length
+userSchema
+    .virtual('friendCount')
+    .get(function (this: IUser) {
+        return this.friends.length;
+    });
+
+const User = model('user', userSchema);
+
+export default User;
