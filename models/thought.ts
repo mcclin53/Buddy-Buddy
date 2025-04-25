cd44925759add80b3019161c3b823915fdab25c6
mcclin53 <dan.mcclintic@gmail.com>
2025-04-25 11:45:24 -0400

    completed reaction and thought schemas

--- models/thought.ts
+++ models/thought.ts
@@ -1,5 +1,12 @@
 import { Schema, Document, model, ObjectId } from 'mongoose';
 
+interface IReaction extends Document {
+    reactionID: ObjectId;
+    reactionBody: String;
+    username: String;
+    createdAt: String;
+}
+
 interface IThought extends Document {
     _id: ObjectId;
     thoughtText: string;
@@ -8,19 +15,46 @@ interface IThought extends Document {
     reactions: IReaction[];
 }
 
-interface IReaction extends Document {
-    reaction: String;
-}
- const reactionSchema = new Schema<IReaction>
+const reactionSchema = new Schema<IReaction>(
+    {
+        reactionID: { type: Schema.Types.ObjectId, default: () => new Types.ObjectId() },
+        reactionBody: { type: String, required: true, maxlength: 280},
+        username: { type: String, required: true },
+        createdAt: {
+            type: Date,
+            default: Date.now, 
+            get: function (timestamp) {
+                return new Date(timestamp).toLocaleString();
+            }
+        },
+    }
+);
+
+reactionSchema.set('toObject', { getters: true });
+reactionSchema.set('toJSON', { getters: true });
+
 const thoughtSchema = new Schema<IThought>(
     {
-        thoughtText: {type: String, required: true, },
-        createdAt: {type: Date, },
+        thoughtText: {type: String, required: true, maxlength: 280},
+        createdAt: {
+            type: Date,
+            default: Date.now, 
+            get: function (timestamp) {
+                return new Date(timestamp).toLocaleString();
+            }
+        },
         username: {type: String, required: true},
         reactions: [
             {
                 type: Schema.Types.ObjectId,
-                ref: 'reactions'
+                ref: 'Reaction'
             }
         ],
     }
+);
+
+thoughtSchema.set('toObject', { getters: true });
+thoughtSchema.set('toJSON', { getters: true });
+
+const Reaction = model<IReaction>('Reaction', reactionSchema);
+const Thought = model<IThought>('Thought', thoughtSchema);
